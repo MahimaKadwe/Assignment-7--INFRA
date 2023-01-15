a3c89e9408e6d44cd5ed7f0a52b2503d15664b23
Mahima Kadwe <mahimakadwe@gmail.com>
2023-01-15 14:28:51 +0530

    first commit

--- mongodb.js
+++ mongodb.js
@@ -5,6 +5,7 @@
 
  app.use(express.json())
  var database
+ var age
 
  app.get('/', (req, resp) => {
 resp.send('Welcome to mongodb APi')
@@ -15,11 +16,53 @@ database.collection('startup_log').find({}).toArray((err, result) => {
         resp.send(result)
     })   
 })
+app.get('/api/startup_log/:id',(req, resp) => {
+    database.collection('startup_log').find({id: parseInt(req.params.id)}).toArray((err, result) => {
+        if(err) throw err
+        resp.send(result)
+    })
+})
 
+app.post('/api/startup_log/addstartup_log' , (req, resp) => {
+    let res = database.collection('startup_log').find({}).sort({id: -1}).limit(1)
+    res.forEach(obj => {
+        if(obj){
+            let startup_log = {
+                id: obj.id +1,
+                name: req.body.name
+            }
+            database.collection('startup_log').insertOne(startup_log, (err, result) =>{
+                if(err) resp.status(500).send(err)
+                resp.send("Added Successfully")
+            })
+        }
+    })
+}) 
+
+app.put('/api/startup_log/:id',(req, resp) => {
+       let startup_log = {
+            id: parseInt(req.params.id),
+            name: req.body.name
+        }
+        database.collection('startup_log').updateOne(
+            {id: parseInt(req.params.id)}, 
+            {$set: startup_log}, (err, result) =>{
+            if(err) throw err
+            resp.send(startup_log)
+        })
+    })
+
+    app.delete('/api/startup_log/:id', (req, resp) => {
+            database.collection('startup_log').deleteOne({id: parseInt(req.params.id)}, (err, result) =>{
+                if(err) throw err
+                resp.send('Data is deleted')
+            })
+        })
+        
  app.listen(8080, () => {
     MongoClient.connect('mongodb://127.0.0.1:27017/demo', { useNewUrlParser: true}, (error, result) => {
     if(error) throw error
-    database = result.db('local')
+    database = result.db('local'),
     console.log('Connection successful')
  })
  
